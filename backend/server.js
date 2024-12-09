14ef8aa2fb7d705b9be6c5d684f4a742890ae2a8
Alex <alexciacciarella@gmail.com>
2024-12-09 18:12:16 +0100

    Ho aggiunto cose al backend, backup pre scossa elettrica del dio Thor

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,27 @@
+//Backend ServerJS
+//For Rest Operations and Server Mantenance
 const express = require("express");
 const cors = require("cors");
 const PORT = 3001;
+//For DB 
+const neo4j = require("neo4j-driver");
+const driver = neo4j.driver('neo4j://localhost:7687', neo4j.auth.basic('neo4j', 'letmein')) 
+const session = driver.session()
 
 const app = express();
 
 app.use(cors());
+
+
+session.executeRead((tx) => {
+  return tx.run('MATCH (n) RETURN count(n) AS count')
+    .then((res) => {
+      console.log(res.records[0].get('count'))
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+})
 
 
 app.get("/articles", (req, res) => {
@@ -89,6 +106,78 @@ app.get("/articles", (req, res) => {
             publishedDate: '2010-01-01',
             bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come un lupo si è impegnato a salvare un villaggio da una violenta tempesta di neve. Il testo descrive le sue azioni e i suoi tentativi per salvare il villaggio.',
             tags: ['lupo', 'animali', 'villaggio']
+          },
+          {
+            id: 10,
+            image: 'https://t4.ftcdn.net/jpg/00/38/34/37/360_F_38343743_lOX7Y1W41vXWX2PzHNI9tzFrwfY929Le.jpg',
+            title: 'Il lupo che ha salvato il villaggio',
+            description: 'Un lupo ha salvato un villaggio da una violenta tempesta di neve. La notte era buia e il vento era forte, e molti abitanti del villaggio stavano rischiando in pericolo.',
+            publishedDate: '2022-01-01',
+            bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come un lupo si è impegnato a salvare un villaggio da una violenta tempesta di neve. Il testo descrive le sue azioni e i suoi tentativi per salvare il villaggio.',
+            tags: ['lupo', 'animali', 'villaggio']
+          },
+          {
+            id: 11,
+            image: 'https://picsum.photos/200/300',
+            title: 'L\'Italia si aggiudica il mondiale del basket',
+            description: 'Una squadra di giocatori italiani ha vinto il mondiale di basket, dopo un\'annata di lavoro e sacrificio. I protagonisti sono stati il guardia tiratore Andrea Gigante e il centro Alessandro Chiti, che hanno guidato la squadra ad una finale sfida contro gli Stati Uniti, che però sono stati i campionati.',
+            publishedDate: '2023-01-01',
+            bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come l\'Italia si è aggiudicata il campionato mondiale di basket. Il testo è molto dettagliato e descrive le azioni dei giocatori e il loro ruolo nel successo della squadra.',
+            tags: ['bruschetta', 'non ci credo']
+          },
+          {
+            id: 12,
+            image: 'https://picsum.photos/200/300?random=1',
+            title: 'Il pensionato sconosciuto che ha salvato il gatto',
+            description: 'Un uomo di mezza età si è impegnato a seguire un gatto che era stato abbandonato e si era perso in un quartiere periferico. Il gatto era sparito da diversi giorni e nessuno sapeva dove si trovasse.',
+            publishedDate: '2022-01-01',
+            bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come un pensionato sconosciuto si è impegnato a cercare e salvare un gatto che era stato abbandonato. Il testo descrive le sue azioni e i suoi tentativi per ritrovare il gatto.',
+            tags: ['gatto', 'animali', 'pensionato']
+          },
+          {
+            id: 13,
+            image: 'https://picsum.photos/200/300',
+            title: 'L\'Italia si aggiudica il mondiale del basket',
+            description: 'Una squadra di giocatori italiani ha vinto il mondiale di basket, dopo un\'annata di lavoro e sacrificio. I protagonisti sono stati il guardia tiratore Andrea Gigante e il centro Alessandro Chiti, che hanno guidato la squadra ad una finale sfida contro gli Stati Uniti, che però sono stati i campionati.',
+            publishedDate: '2022-01-01',
+            bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come l\'Italia si è aggiudicata il campionato mondiale di basket. Il testo è molto dettagliato e descrive le azioni dei giocatori e il loro ruolo nel successo della squadra.',
+            tags: ['bruschetta', 'non ci credo']
+          },
+          {
+            id: 14,
+            image: 'https://picsum.photos/200/300',
+            title: 'L\'Italia si aggiudica il mondiale del basket',
+            description: 'Una squadra di giocatori italiani ha vinto il mondiale di basket, dopo un\'annata di lavoro e sacrificio. I protagonisti sono stati il guardia tiratore Andrea Gigante e il centro Alessandro Chiti, che hanno guidato la squadra ad una finale sfida contro gli Stati Uniti, che però sono stati i campionati.',
+            publishedDate: '2022-01-01',
+            bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come l\'Italia si è aggiudicata il campionato mondiale di basket. Il testo è molto dettagliato e descrive le azioni dei giocatori e il loro ruolo nel successo della squadra.',
+            tags: ['bruschetta', 'non ci credo']
+          },
+          {
+            id: 15,
+            image: 'https://picsum.photos/200/300',
+            title: 'L\'Italia si aggiudica il mondiale del basket',
+            description: 'Una squadra di giocatori italiani ha vinto il mondiale di basket, dopo un\'annata di lavoro e sacrificio. I protagonisti sono stati il guardia tiratore Andrea Gigante e il centro Alessandro Chiti, che hanno guidato la squadra ad una finale sfida contro gli Stati Uniti, che però sono stati i campionati.',
+            publishedDate: '2015-01-01',
+            bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come l\'Italia si è aggiudicata il campionato mondiale di basket. Il testo è molto dettagliato e descrive le azioni dei giocatori e il loro ruolo nel successo della squadra.',
+            tags: ['bruschetta', 'non ci credo']
+          },
+          {
+            id: 16,
+            image: 'https://picsum.photos/200/300',
+            title: 'L\'Italia si aggiudica il mondiale del basket',
+            description: 'Una squadra di giocatori italiani ha vinto il mondiale di basket, dopo un\'annata di lavoro e sacrificio. I protagonisti sono stati il guardia tiratore Andrea Gigante e il centro Alessandro Chiti, che hanno guidato la squadra ad una finale sfida contro gli Stati Uniti, che però sono stati i campionati.',
+            publishedDate: '2022-01-01',
+            bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come l\'Italia si è aggiudicata il campionato mondiale di basket. Il testo è molto dettagliato e descrive le azioni dei giocatori e il loro ruolo nel successo della squadra.',
+            tags: ['bruschetta', 'non ci credo']
+          },
+          {
+            id: 17,
+            image: 'https://picsum.photos/200/300',
+            title: 'L\'Italia si aggiudica il mondiale del basket',
+            description: 'Una squadra di giocatori italiani ha vinto il mondiale di basket, dopo un\'annata di lavoro e sacrificio. I protagonisti sono stati il guardia tiratore Andrea Gigante e il centro Alessandro Chiti, che hanno guidato la squadra ad una finale sfida contro gli Stati Uniti, che però sono stati i campionati.',
+            publishedDate: '2017-01-01',
+            bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come l\'Italia si è aggiudicata il campionato mondiale di basket. Il testo è molto dettagliato e descrive le azioni dei giocatori e il loro ruolo nel successo della squadra.',
+            tags: ['bruschetta', 'non ci credo']
           }
         ])
     });
--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -10,163 +10,6 @@ function FrontPage() {
 
   const [articles, setArticles] = useState([]);
 
-  
-
-  const [data, setData] = useState([
-    {
-      id: 1,
-      image: 'https://picsum.photos/200/300',
-      title: 'L\'Italia si aggiudica il mondiale del basket',
-      description: 'Una squadra di giocatori italiani ha vinto il mondiale di basket, dopo un\'annata di lavoro e sacrificio. I protagonisti sono stati il guardia tiratore Andrea Gigante e il centro Alessandro Chiti, che hanno guidato la squadra ad una finale sfida contro gli Stati Uniti, che però sono stati i campionati.',
-      publishedDate: '2022-01-01',
-      bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come l\'Italia si è aggiudicata il campionato mondiale di basket. Il testo è molto dettagliato e descrive le azioni dei giocatori e il loro ruolo nel successo della squadra.',
-      tags: ['bruschetta', 'non ci credo']
-    },
-    {
-      id: 2,
-      image: 'https://picsum.photos/200/300?random=1',
-      title: 'Il pensionato sconosciuto che ha salvato il gatto',
-      description: 'Un uomo di mezza età si è impegnato a seguire un gatto che era stato abbandonato e si era perso in un quartiere periferico. Il gatto era sparito da diversi giorni e nessuno sapeva dove si trovasse.',
-      publishedDate: '2022-01-01',
-      bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come un pensionato sconosciuto si è impegnato a cercare e salvare un gatto che era stato abbandonato. Il testo descrive le sue azioni e i suoi tentativi per ritrovare il gatto.',
-      tags: ['gatto', 'animali', 'pensionato']
-    },
-    {
-      id: 3,
-      image: 'https://t4.ftcdn.net/jpg/00/38/34/37/360_F_38343743_lOX7Y1W41vXWX2PzHNI9tzFrwfY929Le.jpg',
-      title: 'Il lupo che ha salvato il villaggio',
-      description: 'Un lupo ha salvato un villaggio da una violenta tempesta di neve. La notte era buia e il vento era forte, e molti abitanti del villaggio stavano rischiando in pericolo.',
-      publishedDate: '2022-01-01',
-      bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come un lupo si è impegnato a salvare un villaggio da una violenta tempesta di neve. Il testo descrive le sue azioni e i suoi tentativi per salvare il villaggio.',
-      tags: ['lupo', 'animali', 'villaggio']
-    },
-    {
-      id: 4,
-      image: 'https://picsum.photos/200/300',
-      title: 'L\'Italia si aggiudica il mondiale del basket',
-      description: 'Una squadra di giocatori italiani ha vinto il mondiale di basket, dopo un\'annata di lavoro e sacrificio. I protagonisti sono stati il guardia tiratore Andrea Gigante e il centro Alessandro Chiti, che hanno guidato la squadra ad una finale sfida contro gli Stati Uniti, che però sono stati i campionati.',
-      publishedDate: '2022-01-01',
-      bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come l\'Italia si è aggiudicata il campionato mondiale di basket. Il testo è molto dettagliato e descrive le azioni dei giocatori e il loro ruolo nel successo della squadra.',
-      tags: ['basket', 'calcio', 'calcio mondiale']
-    },
-    {
-      id: 5,
-      image: 'https://picsum.photos/200/300',
-      title: 'L\'Italia si aggiudica il mondiale del basket',
-      description: 'Una squadra di giocatori italiani ha vinto il mondiale di basket, dopo un\'annata di lavoro e sacrificio. I protagonisti sono stati il guardia tiratore Andrea Gigante e il centro Alessandro Chiti, che hanno guidato la squadra ad una finale sfida contro gli Stati Uniti, che però sono stati i campionati.',
-      publishedDate: '2022-01-01',
-      bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come l\'Italia si è aggiudicata il campionato mondiale di basket. Il testo è molto dettagliato e descrive le azioni dei giocatori e il loro ruolo nel successo della squadra.',
-      tags: ['basket', 'calcio', 'calcio mondiale']
-    },
-    {
-      id: 6,
-      image: 'https://t4.ftcdn.net/jpg/00/38/34/37/360_F_38343743_lOX7Y1W41vXWX2PzHNI9tzFrwfY929Le.jpg',
-      title: 'Il lupo che ha salvato il villaggio',
-      description: 'Un lupo ha salvato un villaggio da una violenta tempesta di neve. La notte era buia e il vento era forte, e molti abitanti del villaggio stavano rischiando in pericolo.',
-      publishedDate: '2022-01-01',
-      bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come un lupo si è impegnato a salvare un villaggio da una violenta tempesta di neve. Il testo descrive le sue azioni e i suoi tentativi per salvare il villaggio.',
-      tags: ['lupo', 'animali', 'villaggio']
-    },
-    {
-      id: 7,
-      image: 'https://t4.ftcdn.net/jpg/00/38/34/37/360_F_38343743_lOX7Y1W41vXWX2PzHNI9tzFrwfY929Le.jpg',
-      title: 'Il lupo che ha salvato il villaggio',
-      description: 'Un lupo ha salvato un villaggio da una violenta tempesta di neve. La notte era buia e il vento era forte, e molti abitanti del villaggio stavano rischiando in pericolo.',
-      publishedDate: '2022-01-01',
-      bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come un lupo si è impegnato a salvare un villaggio da una violenta tempesta di neve. Il testo descrive le sue azioni e i suoi tentativi per salvare il villaggio.',
-      tags: ['lupo', 'animali', 'villaggio']
-    },
-    {
-      id: 8,
-      image: 'https://t4.ftcdn.net/jpg/00/38/34/37/360_F_38343743_lOX7Y1W41vXWX2PzHNI9tzFrwfY929Le.jpg',
-      title: 'Il lupo che ha salvato il villaggio',
-      description: 'Un lupo ha salvato un villaggio da una violenta tempesta di neve. La notte era buia e il vento era forte, e molti abitanti del villaggio stavano rischiando in pericolo.',
-      publishedDate: '2022-01-01',
-      bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come un lupo si è impegnato a salvare un villaggio da una violenta tempesta di neve. Il testo descrive le sue azioni e i suoi tentativi per salvare il villaggio.',
-      tags: ['lupo', 'animali', 'villaggio']
-    },
-    {
-      id: 9,
-      image: 'https://t4.ftcdn.net/jpg/00/38/34/37/360_F_38343743_lOX7Y1W41vXWX2PzHNI9tzFrwfY929Le.jpg',
-      title: 'Il lupo che ha salvato il villaggio',
-      description: 'Un lupo ha salvato un villaggio da una violenta tempesta di neve. La notte era buia e il vento era forte, e molti abitanti del villaggio stavano rischiando in pericolo.',
-      publishedDate: '2010-01-01',
-      bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come un lupo si è impegnato a salvare un villaggio da una violenta tempesta di neve. Il testo descrive le sue azioni e i suoi tentativi per salvare il villaggio.',
-      tags: ['lupo', 'animali', 'villaggio']
-    },
-    {
-      id: 10,
-      image: 'https://t4.ftcdn.net/jpg/00/38/34/37/360_F_38343743_lOX7Y1W41vXWX2PzHNI9tzFrwfY929Le.jpg',
-      title: 'Il lupo che ha salvato il villaggio',
-      description: 'Un lupo ha salvato un villaggio da una violenta tempesta di neve. La notte era buia e il vento era forte, e molti abitanti del villaggio stavano rischiando in pericolo.',
-      publishedDate: '2022-01-01',
-      bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come un lupo si è impegnato a salvare un villaggio da una violenta tempesta di neve. Il testo descrive le sue azioni e i suoi tentativi per salvare il villaggio.',
-      tags: ['lupo', 'animali', 'villaggio']
-    },
-    {
-      id: 11,
-      image: 'https://picsum.photos/200/300',
-      title: 'L\'Italia si aggiudica il mondiale del basket',
-      description: 'Una squadra di giocatori italiani ha vinto il mondiale di basket, dopo un\'annata di lavoro e sacrificio. I protagonisti sono stati il guardia tiratore Andrea Gigante e il centro Alessandro Chiti, che hanno guidato la squadra ad una finale sfida contro gli Stati Uniti, che però sono stati i campionati.',
-      publishedDate: '2023-01-01',
-      bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come l\'Italia si è aggiudicata il campionato mondiale di basket. Il testo è molto dettagliato e descrive le azioni dei giocatori e il loro ruolo nel successo della squadra.',
-      tags: ['bruschetta', 'non ci credo']
-    },
-    {
-      id: 12,
-      image: 'https://picsum.photos/200/300?random=1',
-      title: 'Il pensionato sconosciuto che ha salvato il gatto',
-      description: 'Un uomo di mezza età si è impegnato a seguire un gatto che era stato abbandonato e si era perso in un quartiere periferico. Il gatto era sparito da diversi giorni e nessuno sapeva dove si trovasse.',
-      publishedDate: '2022-01-01',
-      bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come un pensionato sconosciuto si è impegnato a cercare e salvare un gatto che era stato abbandonato. Il testo descrive le sue azioni e i suoi tentativi per ritrovare il gatto.',
-      tags: ['gatto', 'animali', 'pensionato']
-    },
-    {
-      id: 13,
-      image: 'https://picsum.photos/200/300',
-      title: 'L\'Italia si aggiudica il mondiale del basket',
-      description: 'Una squadra di giocatori italiani ha vinto il mondiale di basket, dopo un\'annata di lavoro e sacrificio. I protagonisti sono stati il guardia tiratore Andrea Gigante e il centro Alessandro Chiti, che hanno guidato la squadra ad una finale sfida contro gli Stati Uniti, che però sono stati i campionati.',
-      publishedDate: '2022-01-01',
-      bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come l\'Italia si è aggiudicata il campionato mondiale di basket. Il testo è molto dettagliato e descrive le azioni dei giocatori e il loro ruolo nel successo della squadra.',
-      tags: ['bruschetta', 'non ci credo']
-    },
-    {
-      id: 14,
-      image: 'https://picsum.photos/200/300',
-      title: 'L\'Italia si aggiudica il mondiale del basket',
-      description: 'Una squadra di giocatori italiani ha vinto il mondiale di basket, dopo un\'annata di lavoro e sacrificio. I protagonisti sono stati il guardia tiratore Andrea Gigante e il centro Alessandro Chiti, che hanno guidato la squadra ad una finale sfida contro gli Stati Uniti, che però sono stati i campionati.',
-      publishedDate: '2022-01-01',
-      bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come l\'Italia si è aggiudicata il campionato mondiale di basket. Il testo è molto dettagliato e descrive le azioni dei giocatori e il loro ruolo nel successo della squadra.',
-      tags: ['bruschetta', 'non ci credo']
-    },
-    {
-      id: 15,
-      image: 'https://picsum.photos/200/300',
-      title: 'L\'Italia si aggiudica il mondiale del basket',
-      description: 'Una squadra di giocatori italiani ha vinto il mondiale di basket, dopo un\'annata di lavoro e sacrificio. I protagonisti sono stati il guardia tiratore Andrea Gigante e il centro Alessandro Chiti, che hanno guidato la squadra ad una finale sfida contro gli Stati Uniti, che però sono stati i campionati.',
-      publishedDate: '2015-01-01',
-      bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come l\'Italia si è aggiudicata il campionato mondiale di basket. Il testo è molto dettagliato e descrive le azioni dei giocatori e il loro ruolo nel successo della squadra.',
-      tags: ['bruschetta', 'non ci credo']
-    },
-    {
-      id: 16,
-      image: 'https://picsum.photos/200/300',
-      title: 'L\'Italia si aggiudica il mondiale del basket',
-      description: 'Una squadra di giocatori italiani ha vinto il mondiale di basket, dopo un\'annata di lavoro e sacrificio. I protagonisti sono stati il guardia tiratore Andrea Gigante e il centro Alessandro Chiti, che hanno guidato la squadra ad una finale sfida contro gli Stati Uniti, che però sono stati i campionati.',
-      publishedDate: '2022-01-01',
-      bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come l\'Italia si è aggiudicata il campionato mondiale di basket. Il testo è molto dettagliato e descrive le azioni dei giocatori e il loro ruolo nel successo della squadra.',
-      tags: ['bruschetta', 'non ci credo']
-    },
-    {
-      id: 17,
-      image: 'https://picsum.photos/200/300',
-      title: 'L\'Italia si aggiudica il mondiale del basket',
-      description: 'Una squadra di giocatori italiani ha vinto il mondiale di basket, dopo un\'annata di lavoro e sacrificio. I protagonisti sono stati il guardia tiratore Andrea Gigante e il centro Alessandro Chiti, che hanno guidato la squadra ad una finale sfida contro gli Stati Uniti, che però sono stati i campionati.',
-      publishedDate: '2017-01-01',
-      bodyPreview: 'Questo è un testo di esempio di un articolo, che parla di come l\'Italia si è aggiudicata il campionato mondiale di basket. Il testo è molto dettagliato e descrive le azioni dei giocatori e il loro ruolo nel successo della squadra.',
-      tags: ['bruschetta', 'non ci credo']
-    }
-  ]);
   const [farticles, fsetArticles] = useState(articles);
   
 
@@ -181,8 +24,6 @@ function FrontPage() {
         if (!response.ok) {
           throw new Error('Network response was not ok ' + response.statusText);
         }
-        console.log('Articles:', articles)
-        console.log('VisibleArticles:', visibleArticles)
         return response.json();
       })
       .then(data => {
@@ -246,25 +87,6 @@ function FrontPage() {
     }
   }, [selectedTag, farticles, currentPage, articlesPerPage, articles]);
 
-
-  
-
-  // Carica altri articoli quando l'utente clicca sul pulsante "Carica Altro"
-/* const handleLoadMore = () => {
-  // Verifica se l'utente è loggato
-  if (isLoggedIn) {
-    // Calcola la lunghezza attuale degli articoli visibili
-    const currentLength = visibleArticles.length;
-    // Calcola gli articoli successivi da caricare
-    const nextArticles = articles.slice(currentLength, currentLength + 5);
-    // Aggiunge gli articoli successivi agli articoli visibili
-    setVisibleArticles([...visibleArticles, ...nextArticles]);
-  } else {
-    // Se l'utente non è loggato, mostra un messaggio di alert e reindirizza alla pagina di login
-    alert('Devi effettuare il log in prima di vedere questo contenuto');
-    navigate('/login');
-  }
-}; */
   
 
   const handleTagClick = (tag) => {
